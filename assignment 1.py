14ce80dff22cddb40819ce91ddeee600a6c2027d
madhumunagala04 <68542991+madhumunagala04@users.noreply.github.com>
2020-07-20 16:26:34 +0530

    Create assignment 1.py

---
+++ assignment 1.py
@@ -0,0 +1,18 @@
+1.what is python? what are the ways to install python?
+python is a general purpose programming language which is introduced by Guido Van Rossum in the year of 1991.python is both procedure oriented and object oriented programming language.it has huge libraries which are inbuilt.Moreover it is easy to understand.
+There are so many ways to install python.but my self i installed anaconda python in this way.
+steps:
+1.first type anaconda python and google it.
+2.next select first web link
+3.next scroll down to bottom of that page and install whatever suitable package
+according to your pc or laptop(windows,linux,macos)
+4.after installation click next next....
+5.finally type anaconda navigator on your start.'
+##in this we need to need to install python.
+2.what are the coding environments?
+coding environments are the environments where we can write the codes and
+where compilation is performed and in that only we can get the output.
+types of coding environments:
+1.text editors(notepad)
+2.IDE's(spyder,pytorch)
+3.notebook environment(jupiter notebook)
